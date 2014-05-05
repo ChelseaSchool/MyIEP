d9627ab104f30b6ee3a95b2ed48057f7057de9c7
Rik Goldman <rikgoldman@gmail.com>
2014-05-05 17:45:22 -0400

    modified about.php to comply with PEAR errors. Check to ensure requires still work

--- about.php
+++ about.php
@@ -1,15 +1,16 @@
 <?php
+
 /** @file
  * @brief 	About the application and developers; link to Chelsea School
+ * @authors		Rik Goldman <rgoldman@chelseaschool.edu, Sabre Goldman, Jason Banks, Alex, James, Paul, Bryan, TJ, Jonathan, Micah, Stephen, Joseph, Sean
+ * @author		M. Nielson
  * @copyright 	2014 Chelsea School 
  * @copyright 	2005 Grasslands Regional Division #6
- * @copyright		This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * @license This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * @authors		Rik Goldman, Sabre Goldman, Jason Banks, Alex, James, Paul, Bryan, TJ, Jonathan, Micah, Stephen, Joseph, Sean
- * @author		M. Nielson
  */
 
 /** @mainpage
@@ -31,9 +32,9 @@ $MINIMUM_AUTHORIZATION_LEVEL = 100;    //anybody
  */
 
 
-define('IPP_PATH','./');
-require_once(IPP_PATH . 'etc/init.php');
-require_once(IPP_PATH . 'include/supporting_functions.php');
+define('IPP_PATH', './');
+require_once 'IPP_PATH' . 'etc/init.php';
+require_once 'IPP_PATH' . 'include/supporting_functions.php';
 
 header('Pragma: no-cache'); //don't cache this page!
 
