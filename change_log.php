831f5066cd719d2391b82a2b83afa20f4c0876da
Rik Goldman <rikgoldman@gmail.com>
2014-05-08 17:28:51 -0400

    changed to @license tag for autodocumentation (from @copyright)

--- change_log.php
+++ change_log.php
@@ -2,7 +2,7 @@
 /** @file
  * @brief 	Attempt to log changes in the system
  * @copyright 	2014 Chelsea School 
- * @copyright		This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * @license		This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
