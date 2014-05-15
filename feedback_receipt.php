dc4c144d00915f09992fd0f008b073761783d5c2
Rik Goldman <rikgoldman@gmail.com>
2014-05-15 17:16:28 -0400

    added feedback receipt

---
+++ feedback_receipt.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'include/mail_functions.php';
+require_once (IPP_PATH . 'etc/init.php');
+require_once (IPP_PATH . 'include/db.php');
+require_once (IPP_PATH . 'include/auth.php');
+require_once (IPP_PATH . 'include/log.php');
+require_once (IPP_PATH . 'include/supporting_functions.php');
+require_once 'include/page_troubleshoot.php';
+
