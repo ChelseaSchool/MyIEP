141929d024f20740482fad3d131d82d622717ae2
Rik Goldman <rikgoldman@gmail.com>
2014-05-27 15:29:07 -0400

    reverted main

--- main.php
+++ main.php
@@ -1,35 +1,5 @@
-<?php
-/** @file 
- *  @brief  main menu
- *  @bug	right hand drop down nav isn't working
- *  @todo
- *  1. Make input fields wider, perhaps
- *  2. figure out which datepicker stuff can go
- *  3. Create nav link back to student
- *  4. Duplicate nav bar for all pages without one (or two)
- */
-//the authorization level for this page!
-$MINIMUM_AUTHORIZATION_LEVEL = 100;
 
-if(isset($system_message)) $system_message = $system_message;
-else $system_message = "";
-
-define('IPP_PATH', './');
-
-/* eGPS required files. */
-require_once(IPP_PATH . 'etc/init.php');
-require_once(IPP_PATH . 'include/db.php');
-require_once(IPP_PATH . 'include/auth.php');
-if ((int)phpversion() < 5) {
-    require_once(IPP_PATH . 'include/fileutils.php'); 
-} //only for pre v5
-require_once(IPP_PATH . 'include/log.php');
-//require_once(IPP_PATH . 'include/navbar.php');
-require_once(IPP_PATH . 'include/supporting_functions.php');
-//require_once(IPP_PATH . 'include/config.inc.php');
-header('Pragma: no-cache'); //don't cache this page!
-
-<?php
+<? php
 /** @file
  *  @brief  main menu
 *  @bug	right hand drop down nav isn't working
@@ -51,8 +21,6 @@ define('IPP_PATH', './');
 require_once(IPP_PATH . 'etc/init.php');
 require_once(IPP_PATH . 'include/db.php');
 require_once(IPP_PATH . 'include/auth.php');
-
-
 require_once(IPP_PATH . 'include/log.php');
 //require_once(IPP_PATH . 'include/navbar.php');
 require_once(IPP_PATH . 'include/supporting_functions.php');
@@ -66,37 +34,9 @@ header('Pragma: no-cache'); //don't cache this page!
 */
 
 
-//begin HASH TEST
-$connection=connectIPPDB();
-if (!$connection) {
-    echo "no connection";
-}
-$query = "SELECT `unencrypted_password` from iep.USERS where `login_name` = {$_POST['LOGIN_NAME']}";
-if (!$query) {
-    echo "query failed";
-}
-$result = mysql_query($query);
-if (!$result) {
-    $error_message = "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error() . "<BR>Query: '$query'<BR>";
-    echo "NULL";
-}
-if (mysql_num_rows($result) <= 0) {
-    $error_message = $error_message . "You are not an authorized support member(" . __FILE__ . ":" . __LINE__ . ")<BR>" ;
-    echo "NULL";
-}
-$row=mysql_fetch_array($result);
-$storedHash=$row['unencrypted_password'];
-echo $storedHash;
-$userPassword = $_POST['PASSWORD'];
-echo $userPassword . "\n";
-$validate=validateHash($userPassword, $storedHash);
-if (!isset ($validate)) {
-    echo "not validated";
-}
-if ($validate == true) echo "The hash validated successfully against the password";
-//END HASH TEST
 
-/*
+
+
 
 if (isset($_POST['LOGIN_NAME']) && isset( $_POST['PASSWORD'] )) {
     if (!validate( $_POST['LOGIN_NAME'] ,  $_POST['PASSWORD'] )) {
@@ -117,7 +57,7 @@ if (isset($_POST['LOGIN_NAME']) && isset( $_POST['PASSWORD'] )) {
         exit();
     }
 }
-*/
+
 ?>
 
 
