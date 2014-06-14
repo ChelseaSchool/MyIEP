13161bf353ce3ed0fb1855a41870feead4ca964b
Rik Goldman <rikgoldman@gmail.com>
2014-06-14 17:50:06 -0400

    partial overhaul of install process

--- install/index.php
+++ install/index.php
@@ -16,140 +16,163 @@
  *  
  *  @license   GPv2 <http://www.gnu.org/licenses/gpl-2.0.html>
  */
-//check if we have an init.php file already...security problem
-if (is_file("../etc/init.php")) {
-    die("To run the install, " . realpath("../etc/init.php") . " must not already exist!");
-}
-
-
-//the authorization level for this page!
-$MINIMUM_AUTHORIZATION_LEVEL = 100;
-
-
+// check if we have an init.php file already...security problem
 /**
- * Path for required files.
+ * diabled for sanity check
+ *
+ * if (is_file("../etc/init.php")) {
+ * die("To run the install, " . realpath("../etc/init.php") . " must not already exist!");
+ * }
  */
 
+// the authorization level for this page!
+$MINIMUM_AUTHORIZATION_LEVEL = 100;
 
-/* eGPS required files. */
-//require_once(IPP_PATH . 'etc/init.php');
-//require_once(IPP_PATH . 'include/db.php');
-//require_once(IPP_PATH . 'include/auth.php');
-//if ((int)phpversion() < 5) { require_once(IPP_PATH . 'include/fileutils.php'); } //only for pre v5
-//require_once(IPP_PATH . 'include/log.php');
-//require_once(IPP_PATH . 'include/navbar.php');
+require_once '../include/supporting_functions.php';
 
-header('Pragma: no-cache'); //don't cache this page!
+/*
+ * Don't Cache this Page
+ */
 
+header ( 'Pragma: no-cache' );
 
-?> 
+?>
 <!DOCTYPE HTML>
-<HTML lang=en>
-<HEAD>
-    <META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-8">
-    <TITLE>MyIEP Installation</TITLE>
-    <style type="text/css" media="screen">
-        <!--
-            @import "<?php echo IPP_PATH;?>layout/greenborders.css";
-        -->
-    </style>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Installation Index">
+<meta name="author" content="Rik Goldman">
+<TITLE>MyIEP Installation</TITLE>
+<!-- Bootstrap core CSS -->
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="../css/jumbotron.css" rel="stylesheet">
+<style type="text/css">
+body {
+	padding-bottom: 70px;
+}
+</style>
 </HEAD>
-    <BODY>
-        <table class="shadow" border="0" cellspacing="0" cellpadding="0" align="center">  
-        <tr>
-          <td class="shadow-topLeft"></td>
-            <td class="shadow-top"></td>
-            <td class="shadow-topRight"></td>
-        </tr>
-        <tr>
-            <td class="shadow-left"></td>
-            <td class="shadow-center" valign="top">
-                <table class="frame" width=620px align=center border="0">
-                    <tr align="Center">
-                    <td><center><img src="./images/banner.png"></center></td>
-                    </tr>
-                    <tr><td>
-                    &nbsp;
-                    </td></tr>
-                    <tr>
-                        <td valign="top">
-                        <div id="main">
+<BODY>
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">MyIEP Install Wizard</a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.php" title="Depency Check">Check
+							Dependencies</a></li>
+					<li><a onclick="history.go(-1);" title="Back a Page"><span
+							class="glyphicon glyphicon-circle-arrow-left"></span></a></li>
+					<li><a href="sprint_feedback.php" title="Leave User Feedback"><span
+							class="glyphicon glyphicon-envelope"></span></a></li>
+					<li><a href="index.php" title="Logout of MyIEP"><span
+							class="glyphicon glyphicon-off"></span></a></li>
+					<li><a href="about.php" title="About MyIEP"><span
+							class="glyphicon glyphicon-info-sign"></span></a></li>
+					<li><a href="help.php" title="Some Help Here"><span
+							class="glyphicon glyphicon-question-sign"></span></a></li>
+				</ul>
+			</div>
+			<!--/.navbar-collapse -->
+		</div>
+	</div>
 
-                        <center><table><tr><td><center><p class="header">- Installation Check Dependencies -</p></center></td></tr></table></center>
-        <BR><center><table width="80%" border="0"><tr><td><ul>
+	<div class="jumbotron">
+		<div class="container">
+			<h1>MyIEP</h1>
+			<h2>
+				Installation: <small>Check Dependencies</small>
+			</h2>
+			<p>MyIEP is a free and open, web-based IEP management system.</p>
+			<p>
+				&copy; 2014 Chelsea School - <a
+					href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPLv2</a>.
+			</p>
 
-        <?php $fail=FALSE; ?>
-                        <?php 
-                          if (!extension_loaded("mysql")) {
-                           echo "<li>Mysql Extensions are not loaded (FAIL)";
-                           $fail=TRUE;
-                          } else {
-                           echo "<li>Mysql Extension is loaded (PASS)";
-                          }
-                          if (!extension_loaded("gd")) {
-                           echo "<li>GD Libraries are not loaded (FAIL)";
-                           $fail=TRUE;
-                          } else {
-                           echo "<li>GD Libraries are loaded (PASS)";
-                          }
-                          if (!extension_loaded("iconv")) {
-                           echo "<li>iconv Libraries are not loaded (FAIL)";
-                           $fail=TRUE;
-                          } else {
-                           echo "<li>iconv Libraries are loaded (PASS)";
-                          }
-                          if (!extension_loaded("pspell")) {
-                           echo "<li>pspell Libraries are not loaded.<BR>&nbsp;&nbsp;You cannot use the spell checking functions. (Not Recommended)";
-                          } else {
-                           echo "<li>pspell Libraries are loaded (PASS)";
-                          }
-if (!@include_once 'Mail.php') {
-    echo "<li>Pear Mail Class is not loaded (Not Recommended)";
+		</div>
+	</div>
+	<div class="container">
+
+		<ul>
+
+			<?php $fail=FALSE; ?>
+			<?php
+if (! extension_loaded ( "mysql" )) {
+    echo "<li><a href=\"http://packages.ubuntu.com/trusty/php5-mysql\" target=\"_target\">Mysql Extensions</a> are not loaded (FAIL)";
+    $fail = TRUE;
+} else {
+    echo "<li>Mysql Extension is loaded (PASS)";
+}
+if (! extension_loaded ( "gd" )) {
+    echo "<li><a href=\"http://en.wikipedia.org/wiki/GD_Graphics_Library\" target=\"_blank\">GD Libraries</a> are not loaded (FAIL)";
+    $fail = TRUE;
+} else {
+    echo "<li>GD Libraries are loaded (PASS)";
+}
+if (! extension_loaded ( "iconv" )) {
+    echo "<li><a href=\"http://www.php.net/manual/en/iconv.installation.php\" target=\"_blank\">iconv Libraries</a> are not loaded (FAIL)";
+    $fail = TRUE;
+} else {
+    echo "<li>iconv Libraries are loaded (PASS)";
+}
+
+if (! @include_once 'Mail.php') {
+    echo "<li><a href=\"http://pear.php.net/package/Mail/redirected\" target=\"_blank\">Pear Mail Class</a> is not loaded (Not Recommended)";
 } else {
     echo "<li>Pear Mail Class is loaded (Recommended)";
 }
-if (!@include_once 'Mail/mime.php') {
-    echo "<li>Pear Mail/Mime Class is not loaded (Not Recommended)";
+if (! @include_once 'Mail/mime.php') {
+    echo "<li><a href=\"http://pear.php.net/package/Mail_Mime/redirected\" target=\"_blank\">Pear Mail/Mime</a> Class is not loaded (Not Recommended)";
 } else {
     echo "<li>Pear Mail/Mime Class is loaded (Recommended)";
 }
-if (!@include_once 'Net/SMTP.php') {
+if (! @include_once 'Net/SMTP.php') {
     echo "<li>Pear Net/SMTP Class is not loaded (Not Recommended)";
 } else {
     echo "<li>Pear Net/SMTP Class is loaded (Recommended)";
 }
-
-?>
-</ul></td></tr></table></center>
-<?php 
-echo "<form enctype=\"multipart/form-data\" action=\"./install/permissions.php" . "\" method=\"post\">";
-echo " <center><input class=\"sbutton\" type=\"submit\" value=\"Next\"";
-if ($fail) {
-    echo " DISABLED"; 
+if (get_magic_quotes_gpc ()) {
+    echo "<li>gpc_magic_quotes is enabled (FAIL)</li>";
+    $fail = TRUE;
+} else {
+    echo "<li>Magic quotes are disabled (PASS)</li>";
 }
-echo "></center>";
-echo "</form>";
-
-                        
 ?>
-</div>
-</td>
-</tr>
-</table>
-</td>
-<td class="shadow-right"></td>   
-</tr>
-<tr>
-<td class="shadow-left">&nbsp;</td>
-<td class="shadow-center">&nbsp;</td>
-<td class="shadow-right">&nbsp;</td>
-</tr>
-<tr>
-<td class="shadow-bottomLeft"></td>
-<td class="shadow-bottom"></td>
-<td class="shadow-bottomRight"></td>
-</tr>
-</table> 
-<center>Copyright &copy; 2014 Chelsea School</center>
-    </BODY>
+</ul>
+<form enctype="multipart/form-data" action="../install/permissions.php" method="post">
+
+<?php
+    if ($fail) {
+        echo "<button class=\"btn pull-right btn-danger\" type=\"submit\"";
+    } else {
+        echo "<button class=\"btn pull-right btn-success\" type=\"submit\" value=\"Next\"";
+    }
+    if ($fail) {
+        echo " disabled>Missing Dependencies</button>";
+    } else {
+        echo ">Continue</button>";
+    }
+    ?></button>
+		</form>
+
+
+
+	</div>
+
+	<script src="js/jquery-2.1.1.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery-ui-1.10.4.custom.min.js"></script>
+</BODY>
 </HTML>
--- install/permissions.php
+++ install/permissions.php
@@ -7,18 +7,14 @@
  * 2. Copyright Block
  */
 
-//check if we have an init.php file already...security problem
-if(is_file("../etc/init.php")) {
-   define('IPP_PATH','../');
-   require_once("../etc/init.php");
-   if(isset($IPP_IS_CONFIGURED) && $IPP_IS_CONFIGURED) {
-      die("The configuration file:" . realpath("../etc/init.php") . " already exists and the IPP_IS_CONFIGURED flag is set. For security reasons you cannot rerun this page. If you want to rerun the install please manually delete the config file.");
-   }
+// check if we have an init.php file already...security problem
+if (is_file("../etc/init.php")) {
+    define('IPP_PATH', '../');
+    require_once ("../etc/init.php");
 }
 
-//the authorization level for this page!
+// the authorization level for this page!
 $MINIMUM_AUTHORIZATION_LEVEL = 100;
-
 
 /**
  * Path for required files.
@@ -27,107 +23,144 @@ $MINIMUM_AUTHORIZATION_LEVEL = 100;
 error_reporting(1);
 
 /* eGPS required files. */
-//require_once(IPP_PATH . 'etc/init.php');
-//require_once(IPP_PATH . 'include/db.php');
-//require_once(IPP_PATH . 'include/auth.php');
-//if ((int)phpversion() < 5) { require_once(IPP_PATH . 'include/fileutils.php'); } //only for pre v5
-//require_once(IPP_PATH . 'include/log.php');
-//require_once(IPP_PATH . 'include/navbar.php');
+// require_once(IPP_PATH . 'etc/init.php');
+// require_once(IPP_PATH . 'include/db.php');
+// require_once(IPP_PATH . 'include/auth.php');
+// if ((int)phpversion() < 5) { require_once(IPP_PATH . 'include/fileutils.php'); } //only for pre v5
+// require_once(IPP_PATH . 'include/log.php');
+// require_once(IPP_PATH . 'include/navbar.php');
 
-header('Pragma: no-cache'); //don't cache this page!
+header('Pragma: no-cache'); // don't cache this page!
 
-
-?> 
+?>
 <!DOCTYPE HTML>
-<HTML lang=en>
+<HTML lang="en">
 <HEAD>
-    <META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-8">
-    <TITLE>MyIEP Installation</TITLE>
-    <style type="text/css" media="screen">
-        <!--
-            @import "../layout/greenborders.css";
-        -->
-    </style>
-    
+<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-8">
+<TITLE>MyIEP Installation: Install Permissions</TITLE>
+<!-- Bootstrap core CSS -->
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="../css/jumbotron.css" rel="stylesheet">
+<style type="text/css">
+body {
+	padding-bottom: 70px;
+}
+</style>
+
+
 </HEAD>
-    <BODY>
-        <table class="shadow" border="0" cellspacing="0" cellpadding="0" align="center">  
-        <tr>
-          <td class="shadow-topLeft"></td>
-            <td class="shadow-top"></td>
-            <td class="shadow-topRight"></td>
-        </tr>
-        <tr>
-            <td class="shadow-left"></td>
-            <td class="shadow-center" valign="top">
-                <table class="frame" width=620px align=center border="0">
-                    <tr align="Center">
-                    <td><center><img src="../images/banner.png"></center></td>
-                    </tr>
-                    <tr><td>
-                    &nbsp;
-                    </td></tr>
-                    <tr>
-                        <td valign="top">
-                        <div id="main">
+<BODY>
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">MyIEP Install Wizard</a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="permissions.php"
+						title="Permission Check">Permission Check</a></li>
+					<li><a onclick="history.go(-1);" title="Back a Page"><span
+							class="glyphicon glyphicon-circle-arrow-left"></span></a></li>
+					<li><a href="sprint_feedback.php" title="Leave User Feedback"><span
+							class="glyphicon glyphicon-envelope"></span></a></li>
+					<li><a href="index.php" title="Logout of MyIEP"><span
+							class="glyphicon glyphicon-off"></span></a></li>
+					<li><a href="about.php" title="About MyIEP"><span
+							class="glyphicon glyphicon-info-sign"></span></a></li>
+					<li><a href="help.php" title="Some Help Here"><span
+							class="glyphicon glyphicon-question-sign"></span></a></li>
+				</ul>
+			</div>
+			<!--/.navbar-collapse -->
+		</div>
+	</div>
 
-                        <center><table><tr><td><center><p class="header">- Installation Permissions -</p></center></td></tr></table></center>
-        <BR><center><table width="80%" border="0"><tr><td>
-        <?php
-               $option = "Alternatively, you may copy the init.dist.php file to init.php and edit manually.<BR><BR>";
-               $fail=FALSE;
-               $file=file("../etc/init.dist.php");
-               if(!$file) {$fail=TRUE; echo "Cannot open init.dist.php configuration file for read (" . realpath("../etc/init.dist.php") . "). Set the file permissions and reload this page<BR>$option";}
-               else {        
-                  $handle=fopen("../etc/init.php","wc");
-                  if(!$handle) { $fail=TRUE; echo "Cannot open init.php configuration file for writing (" . realpath("../etc/init.php") . "). You might need to create this file, set the permissions, and reload this page<BR><BR>('touch init.php' and 'chmod 777 init.php' in a shell from the " . realpath("../etc") . " directory)<BR><BR>$option";} else {
-                     foreach($file as $line) {
-                        fwrite($handle,$line);
-                     }
-                     fclose($handle);
-                  }
-               }
-               
-               if(!$fail) {
-                 echo realpath("../etc/init.php") . " is writable. Copied default install. Click next";
-               }
-                //$fail=FALSE;
-                //if(is_writable("../etc/init.php")) {
-                // echo "Configuration file (../etc/init.php) is not writeable (FAIL)";
-                // $fail=TRUE;
-                //} else {
-                // echo "Configuration file is writeable (PASS)";
-                //}
+	<div class="jumbotron">
+		<div class="container">
+			<h1>MyIEP</h1>
+			<h2>
+				Installation: <small>Install Permissions</small>
+			</h2>
+			<p>MyIEP is a free and open, web-based IEP management system.</p>
+			<p>
+				&copy; 2014 Chelsea School - <a
+					href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPLv2</a>.
+			</p>
 
-                ?>
-                </td></tr></table></center>
-                        <?php 
-   echo "<form enctype=\"multipart/form-data\" action=\"./database.php" . "\" method=\"post\">";
-   echo " <center><input class=\"sbutton\" type=\"submit\" value=\"Next\"";
-   if($fail) echo " DISABLED";
-   echo "></center>";
-   echo "</form>";
+		</div>
+	</div>
+	<div class="container">
+		<?php
 
-                        
-                        ?>
-                        </div>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-            <td class="shadow-right"></td>   
-        </tr>
-        <tr>
-            <td class="shadow-left">&nbsp;</td>
-            <td class="shadow-center">&nbsp;</td>
-            <td class="shadow-right">&nbsp;</td>
-        </tr>
-        <tr>
-            <td class="shadow-bottomLeft"></td>
-            <td class="shadow-bottom"></td>
-            <td class="shadow-bottomRight"></td>
-        </tr>
-        </table> 
-        
-    </BODY>
+if (isset($IPP_IS_CONFIGURED) && $IPP_IS_CONFIGURED) {
+    die("The configuration file:" . realpath("../etc/init.php") . " already exists and the IPP_IS_CONFIGURED flag is set. For security reasons you cannot rerun this page. If you want to rerun the install please manually delete the config file.");
+}
+?>
+
+
+
+
+
+
+
+		<table width="80%" border="0">
+			<?php
+$option = "Alternatively, you may copy the init.dist.php file to init.php and edit manually.<BR><BR>";
+$fail = FALSE;
+$file = file("../etc/init.dist.php");
+if (! $file) {
+    $fail = TRUE;
+    echo "Cannot open init.dist.php configuration file for read (" . realpath("../etc/init.dist.php") . "). Set the file permissions and reload this page<BR>$option";
+} else {
+    $handle = fopen("../etc/init.php", "wc");
+    if (! $handle) {
+        $fail = TRUE;
+        echo "Cannot open init.php configuration file for writing (" . realpath("../etc/init.php") . "). You might need to create this file, set the permissions, and reload this page<BR><BR>('touch init.php' and 'chmod 777 init.php' in a shell from the " . realpath("../etc") . " directory)<BR><BR>$option";
+    } else {
+        foreach ($file as $line) {
+            fwrite($handle, $line);
+        }
+        fclose($handle);
+    }
+}
+
+if (! $fail) {
+    echo realpath("../etc/init.php") . " is writable. Copied default install. Click next.";
+}
+// $fail=FALSE;
+// if(is_writable("../etc/init.php")) {
+// echo "Configuration file (../etc/init.php) is not writeable (FAIL)";
+// $fail=TRUE;
+// } else {
+// echo "Configuration file is writeable (PASS)";
+// }
+
+?>
+
+<?php
+echo "<form enctype=\"multipart/form-data\" action=\"./database.php" . "\" method=\"post\">";
+echo " <button class=\"btn btn-success pull-right\" type=\"submit\" value=\"Next\"";
+if ($fail)
+    echo " disabled></button>";
+echo ">Next</button>";
+echo "</form>";
+
+?>
+
+
+			</div>
+			<script src="js/jquery-2.1.1.js" type="text/javascript"></script>
+			<script src="js/bootstrap.min.js" type="text/javascript"></script>
+			<script type="text/javascript"
+				src="js/jquery-ui-1.10.4.custom.min.js"></script>
+
+</BODY>
 </HTML>
