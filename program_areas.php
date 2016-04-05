810543a3e8934ca273d77f314ac347a27c46f250
Rik Goldman <rikgoldman@gmail.com>
2016-04-05 14:04:00 -0400

    about to updata machine - making sure everything is committed

---
+++ program_areas.php
@@ -0,0 +1,414 @@
+<?php
+
+/** @file
+ * @brief 	display program area
+ * @copyright 	2014 Chelsea School
+ * @copyright 	2005 Grasslands Regional Division #6
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ * @authors		Rik Goldman, Sabre Goldman, Jason Banks, Alex, James, Paul, Bryan, TJ, Jonathan, Micah, Stephen, Joseph
+ * @author		M. Nielson
+ * @todo
+ * 1. filter input
+ * 2. escape output
+ * 3. docblock documentation
+ * 4. beautify?
+ * 5. eliminate legacy UI
+ * 6. Bootstrap theme
+ * 7. This page is unavailable from front end. But we can't find a use for it.
+ */
+
+// the authorization level for this page!
+$MINIMUM_AUTHORIZATION_LEVEL = 100; // everybody
+
+/*
+ * INPUTS: $_GET['student_id']
+ *
+ */
+
+/**
+ * Path for IPP required files.
+ */
+
+$system_message = "";
+
+define ( 'IPP_PATH', './' );
+
+/* eGPS required files. */
+require_once IPP_PATH . 'etc/init.php';
+require_once IPP_PATH . 'include/db.php';
+require_once IPP_PATH . 'include/auth.php';
+require_once IPP_PATH . 'include/log.php';
+require_once IPP_PATH . 'include/user_functions.php';
+require_once IPP_PATH . 'include/navbar.php';
+require_once IPP_PATH . 'include/supporting_functions.php';
+
+header ( 'Pragma: no-cache' ); // don't cache this page!
+
+if (isset ( $_POST ['LOGIN_NAME'] ) && isset ( $_POST ['PASSWORD'] )) {
+	if (! validate ( $_POST ['LOGIN_NAME'], $_POST ['PASSWORD'] )) {
+		$system_message = $system_message . $error_message;
+		IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+		require (IPP_PATH . 'index.php');
+		exit ();
+	}
+} else {
+	if (! validate ()) {
+		$system_message = $system_message . $error_message;
+		IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+		require (IPP_PATH . 'index.php');
+		exit ();
+	}
+}
+// ************* SESSION active past here **************************
+
+$student_id = "";
+if (isset ( $_GET ['student_id'] ))
+	$student_id = $_GET ['student_id'];
+if (isset ( $_POST ['student_id'] ))
+	$student_id = $_POST ['student_id'];
+
+if ($student_id == "") {
+	// we shouldn't be here without a student id.
+	echo "You've entered this page without supplying a valid student id. Fatal, quitting";
+	exit ();
+}
+
+// check permission levels
+$permission_level = getPermissionLevel ( $_SESSION ['egps_username'] );
+if ($permission_level > $MINIMUM_AUTHORIZATION_LEVEL || $permission_level == NULL) {
+	$system_message = $system_message . "You do not have permission to view this page (IP: " . $_SERVER ['REMOTE_ADDR'] . ")";
+	IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+	require (IPP_PATH . 'security_error.php');
+	exit ();
+}
+
+$our_permission = getStudentPermission ( $student_id );
+if ($our_permission == "WRITE" || $our_permission == "ASSIGN" || $our_permission == "ALL") {
+	// we have write permission.
+	$have_write_permission = true;
+} else {
+	$have_write_permission = false;
+}
+
+// ************** validated past here SESSION ACTIVE WRITE PERMISSION CONFIRMED****************
+
+$student_query = "SELECT * FROM student WHERE student_id = " . mysql_real_escape_string ( $student_id );
+$student_result = mysql_query ( $student_query );
+if (! $student_result) {
+	$error_message = $error_message . "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$student_query'<BR>";
+	$system_message = $system_message . $error_message;
+	IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+} else {
+	$student_row = mysql_fetch_array ( $student_result );
+}
+
+if (isset ( $_GET ['add_area'] ) && $have_write_permission) {
+	
+	// check for duplicate...
+	$check_query = "SELECT * FROM program_area WHERE area='" . mysql_real_escape_string ( $_GET ['program_area'] ) . "' AND end_date IS NULL AND student_id=" . mysql_real_escape_string ( $student_id );
+	$check_result = mysql_query ( $check_query );
+	if (! $check_result) {
+		$error_message = $error_message . "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$check_query'<BR>";
+		$system_message = $system_message . $error_message;
+		IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+	} else {
+		if (mysql_num_rows ( $check_result ) > 0) {
+			$check_row = mysql_fetch_array ( $check_result );
+			$system_message = $system_message . "That is already a program area of this student<BR>";
+		} else {
+			$add_query = "INSERT INTO program_area (student_id,area,start_date,end_date) VALUES (" . mysql_real_escape_string ( $student_id ) . ",'" . mysql_real_escape_string ( $_GET ['program_area'] ) . "',NOW(),NULL)";
+			$add_result = mysql_query ( $add_query );
+			if (! $add_result) {
+				$error_message = $error_message . "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$add_query'<BR>";
+				$system_message = $system_message . $error_message;
+				IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+			}
+		}
+	}
+	// $system_message = $system_message . $add_query . "<BR>";
+}
+
+// check if we are deleting some areas...
+if ($_GET ['delete_x'] && $permission_level <= $IPP_MIN_DELETE_PROGRAM_AREA && $have_write_permission) {
+	$delete_query = "DELETE FROM program_area WHERE ";
+	foreach ( $_GET as $key => $value ) {
+		if (preg_match ( '/^(\d)*$/', $key ))
+			$delete_query = $delete_query . "uid=" . $key . " or ";
+	}
+	// strip trailing 'or' and whitespace
+	$delete_query = substr ( $delete_query, 0, - 4 );
+	// $system_message = $system_message . $delete_query . "<BR>";
+	$delete_result = mysql_query ( $delete_query );
+	if (! $delete_result) {
+		$error_message = "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$delete_query'<BR>";
+		$system_message = $system_message . $error_message;
+		IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+	}
+	// $system_message = $system_message . $delete_query . "<BR>";
+}
+
+// check if we are setting some no longer active...
+if ($_GET ['set_not_active'] && $have_write_permission) {
+	$modify_query = "UPDATE program_area SET end_date=NOW() WHERE ";
+	foreach ( $_GET as $key => $value ) {
+		if (preg_match ( '/^(\d)*$/', $key ))
+			$modify_query = $modify_query . "uid=" . $key . " OR ";
+	}
+	// strip trailing 'or' and whitespace
+	$modify_query = substr ( $modify_query, 0, - 4 );
+	// $system_message = $system_message . $modify_query . "<BR>";
+	$modify_result = mysql_query ( $modify_query );
+	if (! $modify_result) {
+		$error_message = "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$modify_query'<BR>";
+		$system_message = $system_message . $error_message;
+		IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+	}
+}
+
+// check if we are setting some no longer active...
+if ($_GET ['set_continue'] && $have_write_permission) {
+	$modify_query = "UPDATE program_area SET end_date=NULL WHERE ";
+	foreach ( $_GET as $key => $value ) {
+		if (preg_match ( '/^(\d)*$/', $key ))
+			$modify_query = $modify_query . "uid=" . $key . " OR ";
+	}
+	// strip trailing 'or' and whitespace
+	$modify_query = substr ( $modify_query, 0, - 4 );
+	// $system_message = $system_message . $modify_query . "<BR>";
+	$modify_result = mysql_query ( $modify_query );
+	if (! $modify_result) {
+		$error_message = "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$modify_query'<BR>";
+		$system_message = $system_message . $error_message;
+		IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+	}
+}
+
+$program_area_query = "SELECT * FROM program_area WHERE student_id=" . mysql_real_escape_string ( $student_id ) . " ORDER BY end_date ASC,start_date DESC";
+$program_area_result = mysql_query ( $program_area_query );
+if (! $program_area_result) {
+	$error_message = $error_message . "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$program_area_query'<BR>";
+	$system_message = $system_message . $error_message;
+	IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+}
+
+/**
+ * ****************** popup chooser support function *****************
+ */
+function createJavaScript($dataSource, $arrayName = 'rows') {
+	// validate variable name
+	if (! is_string ( $arrayName )) {
+		$system_message = $system_message . "Error in popup chooser support function name supplied not a valid string  (" . __FILE__ . ":" . __LINE__ . ")";
+		return FALSE;
+	}
+	
+	// initialize JavaScript string
+	$javascript = '<!--Begin popup array--><script>var ' . $arrayName . '=[];';
+	
+	// check if $dataSource is a file or a result set
+	if (is_file ( $dataSource )) {
+		
+		// read data from file
+		$row = file ( $dataSource );
+		
+		// build JavaScript array
+		for($i = 0; $i < count ( $row ); $i ++) {
+			$javascript .= $arrayName . '[' . $i . ']="' . trim ( $row [$i] ) . '";';
+		}
+	} 	
+
+	// read data from result set
+	else {
+		
+		// check if we have a valid result set
+		if (! $numRows = mysql_num_rows ( $dataSource )) {
+			die ( 'Invalid result set parameter' );
+		}
+		for($i = 0; $i < $numRows; $i ++) {
+			// build JavaScript array from result set
+			$javascript .= $arrayName . '[' . $i . ']="';
+			$tempOutput = '';
+			// output only the first column
+			$row = mysql_fetch_array ( $dataSource );
+			
+			$tempOutput .= $row [0] . ' ';
+			
+			$javascript .= trim ( $tempOutput ) . '";';
+		}
+	}
+	$javascript .= '</script><!--End popup array-->' . "\n";
+	
+	// return JavaScript code
+	return $javascript;
+}
+function echoJSServicesArray() {
+	global $system_message;
+	$coordlist_query = "SELECT DISTINCT `name`, COUNT(`name`) AS `count` FROM typical_long_term_goal_category WHERE is_deleted='N' GROUP BY `name` ORDER BY `name` DESC LIMIT 200";
+	$coordlist_result = mysql_query ( $coordlist_query );
+	if (! $coordlist_result) {
+		$error_message = "Database query failed (" . __FILE__ . ":" . __LINE__ . "): " . mysql_error () . "<BR>Query: '$coordlist_query'<BR>";
+		$system_message = $system_message . $error_message;
+		IPP_LOG ( $system_message, $_SESSION ['egps_username'], 'ERROR' );
+	} else {
+		// call the function to create the javascript array...
+		echo createJavaScript ( $coordlist_result, "popuplist" );
+	}
+}
+/**
+ * ********************** end popup chooser support funtion *****************
+ */
+
+print_html5_primer ();
+?>
+
+
+<script language="javascript"
+    src="<?php echo IPP_PATH . "include/popupchooser.js"; ?>"></script>
+<script language="javascript"
+    src="<?php echo IPP_PATH . "include/autocomplete.js"; ?>"></script>
+<?php
+// output the javascript array for the chooser popup
+echoJSServicesArray ();
+?>
+<SCRIPT LANGUAGE="JavaScript">
+      function confirmChecked() {
+          var szGetVars = "delete_supervisor=";
+          var szConfirmMessage = "Are you sure you want to modify/delete program area(s):\n";
+          var count = 0;
+          form=document.programareahistory;
+          for(var x=0; x<form.elements.length; x++) {
+              if(form.elements[x].type=="checkbox") {
+                  if(form.elements[x].checked) {
+                     szGetVars = szGetVars + form.elements[x].name + "|";
+                     szConfirmMessage = szConfirmMessage + "ID #" + form.elements[x].name + "\n";
+                     count++;
+                  }
+              }
+          }
+          if(!count) { alert("Nothing Selected"); return false; }
+          if(confirm(szConfirmMessage))
+              return true;
+          else
+              return false;
+      }
+
+      function noPermission() {
+          alert("You don't have the permission level necessary"); return false;
+      }
+    </SCRIPT>
+<?php print_bootstrap_head(); ?>
+</HEAD>
+<BODY>
+<?php print_student_navbar($student_id, $student_row['first_name'] . " " . $student_row['last_name']); ?>
+    <?php print_jumbotron_with_page_name("Goal Areas", $student_row['first_name'] . " " . $student_row['last_name'], $our_permission); ?>
+<?php if ($system_message) { echo "<center><table width=\"80%\"><tr><td><p class=\"message\">" . $system_message . "</p></td></tr></table></center>";} ?>
+
+<!-- BEGIN add supervisor -->
+    <center>
+        <form name="addsupervisor" enctype="multipart/form-data"
+            action="<?php echo IPP_PATH . "program_area.php"; ?>"
+            method="get"
+            <?php if(!$have_write_permission) echo "onSubmit=\"return noPermission();\"" ?>>
+            <table border="0" cellspacing="0" cellpadding="0"
+                width="80%">
+                <tr>
+                    <td colspan="3">
+                        <p class="info_text">Edit and click 'Add'.</p> <input
+                        type="hidden" name="add_area" value="1"> <input
+                        type="hidden" name="student_id"
+                        value="<?php echo $student_id; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="bottom" bgcolor="#E0E2F2">Area</td>
+                    <td bgcolor="#E0E2F2"><input type="text"
+                        name="program_area" size="30" maxsize="255"
+                        value="<?php echo $_GET['program_area']; ?>"
+                        onkeypress="return autocomplete(this,event,popuplist)">
+                        &nbsp;<img
+                        src="<?php echo IPP_PATH . "images/choosericon.png"; ?>"
+                        height="17" width="17" border=0
+                        onClick="popUpChooser(this,document.all.program_area)">
+                    </td>
+                    <td valign="center" align="center" bgcolor="#E0E2F2"
+                        rowspan="2"><input type="submit" name="add"
+                        value="add"></td>
+                </tr>
+                <tr>
+                    <td valign="bottom" align="center" bgcolor="#E0E2F2"
+                        colspan="2">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td valign="bottom" align="center" bgcolor="#E0E2F2"
+                        colspan="3">&nbsp;</td>
+                </tr>
+            </table>
+        </form>
+    </center>
+    <!-- END add supervisor -->
+
+    <!-- BEGIN ipp history table -->
+    <form name="programareahistory" onSubmit="return confirmChecked();"
+        enctype="multipart/form-data"
+        action="<?php echo IPP_PATH . "program_area.php"; ?>"
+        method="get">
+        <input type="hidden" name="student_id"
+            value="<?php echo $student_id ?>">
+        <center>
+            <table width="80%" border="0">
+
+                        <?php
+																								$bgcolor = "#DFDFDF";
+																								
+																								// print the header row...
+																								echo "<tr><td bgcolor=\"#E0E2F2\">&nbsp;</td><td bgcolor=\"#E0E2F2\">UID</td><td align=\"center\" bgcolor=\"#E0E2F2\">Area</td><td align=\"center\" bgcolor=\"#E0E2F2\">Start Date</td><td align=\"center\" bgcolor=\"#E0E2F2\">End Date</td></tr>\n";
+																								while ( $program_area_row = mysql_fetch_array ( $program_area_result ) ) { // current...
+																									echo "<tr>\n";
+																									echo "<td bgcolor=\"#E0E2F2\"><input type=\"checkbox\" name=\"" . $program_area_row ['uid'] . "\"></td>";
+																									echo "<td bgcolor=\"$bgcolor\">" . $program_area_row ['uid'] . "</td>";
+																									echo "<td bgcolor=\"$bgcolor\">" . $program_area_row ['area'] . "</td>\n";
+																									echo "<td bgcolor=\"$bgcolor\">" . $program_area_row ['start_date'] . "</td>\n";
+																									if ($program_area_row ['end_date'] == "")
+																										echo "<td bgcolor=\"$bgcolor\">-Current-</td>\n";
+																									else
+																										echo "<td bgcolor=\"$bgcolor\">" . $program_area_row ['end_date'] . "</td>\n";
+																									echo "</tr>\n";
+																									if ($bgcolor == "#DFDFDF")
+																										$bgcolor = "#CCCCCC";
+																									else
+																										$bgcolor = "#DFDFDF";
+																								}
+																								?>
+                        <tr>
+                    <td colspan="6" align="left">
+                        <table>
+                            <tr>
+                                <td nowrap><img
+                                    src="<?php echo IPP_PATH . "images/table_arrow.png"; ?>">&nbsp;With
+                                    Selected:</td>
+                                <td>
+                             <?php
+																													if ($have_write_permission) {
+																														echo "<INPUT NAME=\"set_not_active\" TYPE=\"image\" SRC=\"" . IPP_PATH . "images/smallbutton.php?title=End\" border=\"0\" value=\"set_not_active\">";
+																														echo "<INPUT NAME=\"set_continue\" TYPE=\"image\" SRC=\"" . IPP_PATH . "images/smallbutton.php?title=Continue\" border=\"0\" value=\"set_continue\">";
+																													}
+																													// if we have permissions also allow delete and set all.
+																													if ($permission_level <= $IPP_MIN_DELETE_PROGRAM_AREA && $have_write_permission) {
+																														echo "<INPUT NAME=\"delete\" TYPE=\"image\" SRC=\"" . IPP_PATH . "images/smallbutton.php?title=Delete\" border=\"0\" value=\"delete\">";
+																													}
+																													?>
+                             </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </form>
+
+
+
+    </table>
+
+    <?php print_bootstrap_js(); ?>
+</BODY>
+</HTML>
